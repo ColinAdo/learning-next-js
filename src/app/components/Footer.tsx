a876498976e6f67306bf162047988a1b7097b7ee
ColinAdo <colinadodev30@gmail.com>
2024-05-05 11:10:14 +0300

    Add working with layouts

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <>
+            <p
+                style={{
+                    background: 'blue',
+                    padding: '1rem',
+                }}
+            >Footer</p>
+        </>
+    );
+}
