bd41d36804b6087a547b3737e836c0172bd04212
ColinAdo <colinadodev30@gmail.com>
2024-05-17 17:24:39 +0300

    Add working with URL query parameters fro searching

--- src/app/comments/route.tsx
+++ src/app/comments/route.tsx
@@ -1,7 +1,15 @@
+import { type NextRequest } from 'next/server';
 import { comments } from './data';
 
-export async function GET() {
-    return Response.json(comments);
+export async function GET(request: NextRequest) {
+    const searchParam = request.nextUrl.searchParams;
+    const query = searchParam.get('query');
+    
+    const searchedComment = query ? comments.filter(
+        (comment) => comment.text.includes(query)
+    ) : comments;
+
+    return Response.json(searchedComment);
 }
 
 export async function POST(request: Request) { 
