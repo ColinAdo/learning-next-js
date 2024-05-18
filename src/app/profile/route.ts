369f37a9fc0bab0a5bd58a2eff99dec34774abca
ColinAdo <colinadodev30@gmail.com>
2024-05-18 20:24:25 +0300

    Add working with headers in route hanlders

---
+++ src/app/profile/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headersList = headers();
+
+    console.log(requestHeaders.get('Authorization'));
+    console.log(headersList.get('Authorization'));
+
+    return new Response("Profile GET");
+}
