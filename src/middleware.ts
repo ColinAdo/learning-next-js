eccb6021f71fef3120b74bee1eb52fd9f709fadf
ColinAdo <colinadodev30@gmail.com>
2024-05-20 14:50:38 +0300

    Add middleware using conditional statement

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
+// Custom matcher config
+// export function middleware(request: NextRequest) {
+
+//     return NextResponse.redirect(new URL("/", request.url))
+// }
+
+// export const config = {
+//     matcher: "/profile",
+// };
+
+
+// Using conditional statement
 export function middleware(request: NextRequest) {
 
-    return NextResponse.redirect(new URL("/", request.url))
+    if (request.nextUrl.pathname === "/profile") { 
+        return NextResponse.redirect(new URL("/comments", request.url))
+    }
 }
 
-export const config = {
-    matcher: "/profile",
-};
